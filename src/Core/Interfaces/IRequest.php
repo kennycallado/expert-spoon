d7e290a07e45e47dc56b20fa75e5d39338610803
kenny callado <kennycallado@hotmail.com>
2021-11-04 10:34:07 +0100

    Mejora Ahora las rutas manejan los verbos de la petición

---
+++ src/Core/Interfaces/IRequest.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Core\Interfaces;
+
+interface IRequest{
+  public function getRoute();
+  public function getParams();
+  /* nuevos métodos */
+  public function getMethod();
+  public function getRequestBody();
+}
