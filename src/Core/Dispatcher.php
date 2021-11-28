688796df1ab9ee24edc968cbb8526208096dfd31
kenny callado <kennycallado@hotmail.com>
2021-11-28 13:24:59 +0100

    feature acepta variable tipo  _:id_ en la definición de la ruta

--- src/Core/Dispatcher.php
+++ src/Core/Dispatcher.php
@@ -13,30 +13,58 @@ class Dispatcher
   {
     $this->routeList = $routeCollection->getRoutes();
     $this->currentRequest = $request;
+
     $this->dispatch();
   }
+
+  private function action($method, $route, $param = null)
+  {
+    $controllerClass = "App\\Controllers\\" . $this->routeList[$method][$route]["controller"];
+    $controller =  new $controllerClass;
+    $action = $this->routeList[$method][$route]["action"];
+
+    /* Si llega petición POST obtiene el body */
+    /* y lo pasa como primer parámetro despues resto */
+    /* para peticiones PUT u otras deberia crear elfeif */
+    if ($method == "POST") {
+      $body = $this->currentRequest->getPostBody();
+      $controller->$action($body, $param);
+      /* sino pasa los parametros normalmente */
+    } else $controller->$action($param);
+  }
+
   private function dispatch()
   {
-    /* Guardamos el método de la petición */
     $method = $this->currentRequest->getMethod();
     $route = $this->currentRequest->getRoute();
+    $uri = $this->currentRequest->getUri();
 
-    /* Ahora el routeList contiene [method][route] */
-    if (isset($this->routeList[$method][$route])) {
-      $controllerClass = "App\\Controllers\\" . $this->routeList[$method][$route]["controller"];
-      $controller =  new $controllerClass;
-      $action = $this->routeList[$method][$route]["action"];
+    /* en caso de /:id y cosas así... tengo que manejar al final */
+    if ($uri === "/") $this->action($method, $route);
 
-      /* Si llega petición POST obtiene el body */
-      /* y lo pasa como primer parámetro despues resto */
-      /* para peticiones PUT u otras deberia crear elfeif */
-      if ($method == "POST") {
-        $body = $this->currentRequest->getPostBody();
-        $controller->$action($body, ...$this->currentRequest->getParams());
-        /* sino pasa los parametros normalmente */
-      } else $controller->$action(...$this->currentRequest->getParams());
-    } else {
-      echo "ruta no existe";
+    /* Itera por el array para cotejar */
+    $uriArray = array_filter(explode("/", $uri));
+    foreach ($this->routeList[$method] as $route => $object) {
+      /* la ruta es completa y no tiene varibales */
+      if ($uri === $route) {
+        $this->action($method, $route);
+
+        return;
+      }
+
+      $routeArray = array_filter(explode("/", $route));
+      if (count($uriArray) === count($routeArray)) {
+        for ($i = 1; $i <= count($routeArray); $i++) {
+          if ($routeArray[$i] === $uriArray[$i]) {
+            if (isset($routeArray[$i + 1]) && str_contains($routeArray[$i + 1], ":")) {
+              $paramName = substr($routeArray[$i + 1], 1);
+              $this->action($method, $route, ["$paramName" => $uriArray[$i + 1]]);
+
+              return;
+            }
+          }
+        }
+      }
     }
   }
 }
